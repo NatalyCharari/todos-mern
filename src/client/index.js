8c564a90fd56076a429c490af544fd6fd981f422
Nataly Charari <nathalie.charari@gmail.com>
2020-05-02 19:55:07 +0200

    Rendered App inside redux provider

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-import App from './App'
+import App from './App';
+import rootReducer from './reducers/rootReducer';
 
-render(<App/>, document.getElementById('app'))
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
