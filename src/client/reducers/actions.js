28e63b58ac10721a1f39b42290a4663a96891965
Nataly Charari <nathalie.charari@gmail.com>
2020-05-02 21:59:36 +0200

    Updated actions and reducer

--- src/client/reducers/actions.js
+++ src/client/reducers/actions.js
@@ -1,10 +1,22 @@
 import { getTodos } from '../api/todo';
+import { login } from '../api/user';
 
-export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+export const FETCH_TODOS_SUCCESS = 'FETCH_TODOS_SUCCESS';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const REMOVE_TOKEN = 'REMOVE_TOKEN';
 
-export const fetchDataSuccess = (data) => ({
-  type: FETCH_DATA_SUCCESS,
-  payload: data,
+const fetchTodosSuccess = (todos) => ({
+  type: FETCH_TODOS_SUCCESS,
+  payload: { todos },
+});
+
+const loginSuccess = ({ token }) => ({
+  type: LOGIN_SUCCESS,
+  payload: { token },
+});
+
+export const removeToken = () => ({
+  type: REMOVE_TOKEN,
 });
 
 const handleErrors = (error) => {
@@ -21,22 +33,24 @@ const handleErrors = (error) => {
   }
 };
 
-const handleResponse = (request) => (dispatch) =>
+const handleResponse = (request, action) => (dispatch) =>
   request
     .then((response) => response.json())
     .then((data) => {
-      dispatch(fetchDataSuccess(data));
+      console.log(data);
+      dispatch(action(data));
     })
     .catch(handleErrors);
 
-const fetchData = (request, data) => (dispatch) => {
+const fetchData = (request, action, data = null) => (dispatch) => {
   if (data) {
-    return dispatch(handleResponse(request(data)));
+    return dispatch(handleResponse(request(data), action));
   } else {
-    return dispatch(handleResponse(request()));
+    return dispatch(handleResponse(request(), action));
   }
 };
 
-export const fetchsTodos = (token) => {
-  return fetchData(getTodos, token);
-};
+export const fetchTodos = (token) =>
+  fetchData(getTodos, fetchTodosSuccess, token);
+
+export const loginUser = (user) => fetchData(login, loginSuccess, user);
--- src/client/reducers/reducer.js
+++ src/client/reducers/reducer.js
@@ -1,16 +1,28 @@
-import { FETCH_DATA_SUCCESS } from './actions';
+import { FETCH_TODOS_SUCCESS, LOGIN_SUCCESS, REMOVE_TOKEN } from './actions';
 
 const initialState = {
+  title: 'Todo List',
+  token: null,
   todos: [],
-  currentTodo: {},
+  currentTodo: null,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_DATA_SUCCESS:
+    case FETCH_TODOS_SUCCESS:
       return {
         ...state,
-        todos: action.payload,
+        todos: action.payload.todos,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        token: action.payload.token,
+      };
+    case REMOVE_TOKEN:
+      return {
+        ...state,
+        token: null,
       };
     default:
       return state;
