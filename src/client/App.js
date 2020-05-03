2e020b644928a6cf9fd20d1a739705724d9ed230
Nataly Charari <nathalie.charari@gmail.com>
2020-05-03 14:08:49 +0200

    Removed last unused api call in App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,40 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import Navigation from './components/Navigation';
-
 import { removeToken } from './reducers/actions';
 
+import Navigation from './components/Navigation';
 import LoginForm from './containers/LoginForm';
 import TodosPage from './containers/TodosPage';
 import EditionForm from './containers/EditionForm';
 import AddForm from './containers/AddForm';
 
 const App = ({ currentTodo, logout, title, token }) => {
-  /*constructor(props) {
-    super(props);
-
-    this.removeTodo = this.removeTodo.bind(this);
-  }*/
-
-  /*removeTodo(id) {
-    fetch(`/api/todos/${id}`, {
-      method: 'DELETE',
-      headers: {
-        authorization: 'bearer ' + localStorage.getItem('token'),
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const newTodos = this.state.todos.filter((todo) => {
-          return todo._id != id;
-        });
-        localStorage.setItem('todos', JSON.stringify(newTodos));
-        this.setState({ todos: newTodos });
-      })
-      .catch((err) => console.log('err', err));
-  }*/
-
   return (
     <div>
       <Navigation title={title} onLogout={logout} />
